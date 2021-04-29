b8b97209e93d4d039fe120cf00b3ac2553ab69a3
Jake Murphy <murph9y@gmail.com>
2021-04-29 23:10:43 +1000

    add state, fix content type

--- base/site_contents/s3_fill.tf
+++ base/site_contents/s3_fill.tf
@@ -3,4 +3,5 @@ resource "aws_s3_bucket_object" "site_file" {
     bucket = var.s3_bucket_id
     source = "${path.module}/index.html"
     etag = filemd5("${path.module}/index.html")
+    content_type = "text/html"
 }
